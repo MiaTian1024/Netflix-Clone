f754e0118d11e27e3a2828e5ded03ef78cb6b523
MiaTian1024 <sddxtm123@gmail.com>
2023-06-30 18:01:51 +1200

    google login and github login completed

--- netflix-clone/app/api/auth/[...nextauth]/route.ts
+++ netflix-clone/app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,25 @@
-import NextAuth from "next-auth/next";
-import Credentials from "next-auth/providers/credentials";
+import NextAuth, { AuthOptions } from "next-auth"
+import CredentialsProvider from "next-auth/providers/credentials"
 import { compare } from "bcrypt";
 
-import prismadb from '@/lib/prismadb'
+import GithubProvider from 'next-auth/providers/github';
+import GoogleProvider from 'next-auth/providers/google';
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
 
-const handler = NextAuth({
+import prisma from '@/lib/prismadb'
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
     providers: [
-        Credentials({
+        GithubProvider({
+            clientId:process.env.GITHUB_ID as string,
+            clientSecret:process.env.GITHUB_SECRET as string
+        }),
+        GoogleProvider({
+            clientId:process.env.GOOGLE_CLIENT_ID as string,
+            clientSecret:process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        CredentialsProvider({
             id: 'credentials',
             name: 'Credentials',
             credentials: {
@@ -53,6 +66,8 @@ const handler = NextAuth({
         secret: process.env.NEXTAUTH_JWT_SECRET,
     },
     secret: process.env.NEXTAUTH_SECRET,
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST}
